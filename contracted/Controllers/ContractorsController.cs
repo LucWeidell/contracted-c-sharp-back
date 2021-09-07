1b54694d4fe07738a635946a23f3d39d2f341f84
Luc Weidell <>
2021-09-07 16:56:13 -0600

    contractors controller and service in prog

--- contracted/Controllers/ContractorsController.cs
+++ contracted/Controllers/ContractorsController.cs
@@ -1,7 +1,35 @@
+using System.Collections.Generic;
+using contracted.Models;
+using contracted.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace contracted.Controllers
 {
+    [ApiController]
+    [Route("/api/[controller]")]
+
     public class ContractorsController
     {
-        
-    }
+        private readonly ContractorsService _cs;
+
+        public ContractorsController(ContractorsService cs)
+        {
+            _cs = cs;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Contractor>> Get()
+        {
+            try
+            {
+                await _cs.Get();
+                List<Contractor> contractors = _cs.Get();
+                return Ok(ContractorsController);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message)
+            }
+        }
+  }
 }
--- contracted/Services/ContractorsService.cs
+++ contracted/Services/ContractorsService.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+using contracted.Models;
+using contracted.Repositories;
+
 namespace contracted.Services
 {
-    public class ContractorsService
+  public class ContractorsService
     {
-        
+        private readonly ContractorsRepository _repo;
+
+    public ContractorsService(ContractorsRepository repo)
+    {
+      _repo = repo;
     }
+
+    internal List<Contractor> Get()
+    {
+        List<Contractor> contractors = _repo.GetAll();
+        if(contractors == null)
+        {
+            throw new BadRequest("Failed getAll");
+        }
+        return contractors;
+    }
+  }
 }
